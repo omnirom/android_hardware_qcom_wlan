91b96be4200df5b7f748536f5f32e1624fe6b826
micky387 <mickaelsaibi@free.fr>
2024-03-19 20:53:29 -0400

    Wlan: Fix duplicate include with AOSP Wlan
    wpa_supplicant_conf.mk is already called by the AOSP Wlan repo.
    
    Rename lib_driver_cmd_qcwcn too (Need change in DT)
    
    Change-Id: I760430550fd6e62474e48b608471bdcc898b3b0e

--- qcwcn/config/Android.mk
+++ qcwcn/config/Android.mk
@@ -19,6 +19,6 @@ LOCAL_PATH := $(call my-dir)
 
 WIFI_DRIVER_SOCKET_IFACE := wlan0
 ifeq ($(strip $(WPA_SUPPLICANT_VERSION)),VER_0_8_X)
-  include external/wpa_supplicant_8/wpa_supplicant/wpa_supplicant_conf.mk
+  #include external/wpa_supplicant_8/wpa_supplicant/wpa_supplicant_conf.mk
 endif
 #######################
